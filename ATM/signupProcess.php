b8cdc9c1ad7cf713964e1043cc6f7b8d1aa964bb
Jinseok Heo <genie.h1@gmail.com>
2020-11-06 20:13:03 -0800

    Backend - Simple Sign Up System Github Markdown syntax update

---
+++ ATM/signupProcess.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sign Up</title>
+  </head>
+  <body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "onlineatm";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["phone"]) && isset($_POST["ssn"])) {
+      // collect value of input field
+      $email = $_POST['email'];
+      $pw = $_POST['password'];
+      $fname = $_POST['fname'];
+      $lname = $_POST['lname'];
+      $phone = $_POST['phone'];
+      $ssn = $_POST['ssn'];
+
+      $sql = "INSERT INTO accounts (email, password, firstname, lastname, phone, ssn) VALUES ('$email', '$pw', '$fname', '$lname', '$phone', '$ssn')";
+
+      if ($conn->query($sql) === TRUE) {
+        $message = "Your account successfully created.";
+        echo "<script>alert('$message');</script>";
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+    } else {
+      echo "The form does not filled yet.";
+    }
+    mysqli_close($conn);
+    ?>
+
+    <div class="back">
+      <p>Back to the log in page.</p>
+      <button type="button" onclick="window.location.href = &quot;../index.html&quot;;">Log In</button>
+    </div>
+  </body>
+</html>
--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 # GroupProject
 SJSU CMPE131 Group4
 
-<h3> Database Setting </h3>
-<p>DB name: onlineatm</p>
-<p>table name: accounts</p>
-<p>column:</p>
-- email: Unique (since it is our ID) <br>
-- password <br>
-- firstname <br>
-- lastname <br>
-- phone <br>
-- ssn <br>
-- balance: INT <br>
+### Database Setting###
+DB name: onlineatm\
+table name: accounts\
+column:\
+*email: Unique (since it is our ID) <br>
+*password <br>
+*firstname <br>
+*lastname <br>
+*phone <br>
+*ssn <br>
+*balance: INT <br>
 
-<h3>Creating DB through SQL</h3>
-<p>CREATE DATABASE onlineatm</p>
-<p>CREATE TABLE MyGuests (
-email VARCHAR(255) NOT NULL  PRIMARY KEY,
-password VARCHAR(255) NOT NULL,
-firstname VARCHAR(255) NOT NULL,
-lastname VARCHAR(255) NOT NULL,
-phone VARCHAR(255) NOT NULL,
-ssn VARCHAR(255) NOT NULL
-)</p>
+###Creating DB through SQL###
+CREATE DATABASE onlineatm\
+CREATE TABLE MyGuests (\
+email VARCHAR(255) NOT NULL  PRIMARY KEY,\
+password VARCHAR(255) NOT NULL,\
+firstname VARCHAR(255) NOT NULL,\
+lastname VARCHAR(255) NOT NULL,\
+phone VARCHAR(255) NOT NULL,\
+ssn VARCHAR(255) NOT NULL\
+)\
 
-<h3>Inserting data</h3>
-<p>INSERT INTO table_name (column1, column2, column3,...) VALUES (value1, value2, value3,...)</p>
+###Inserting data###
+INSERT INTO table_name (column1, column2, column3,...) VALUES (value1, value2, value3,...)\
